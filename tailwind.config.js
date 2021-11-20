5fdb8bfcd0843b19e40d4c64803c21b779eb5502
Stanislav Katyushin <justtrace17@gmail.com>
2021-11-20 23:13:09 +0300

    starting project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      tablet: "768px",
+      desktop: "1440px",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

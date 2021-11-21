6c980e7f631fa101b4cd757b97fecb402c54ec1d
Stanislav Katyushin <justtrace17@gmail.com>
2021-11-21 04:00:40 +0300

    added header, subheader and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
       tablet: "768px",
       desktop: "1440px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        bord: "#D1D2D6",
+        main: "#1A1B22",
+        tab: "rgba(26, 27, 34, 0.5)",
+        bgclr: "#F5F6F7",
+        gry: "#B6BCBF",
+      },
+      fontFamily: {
+        slab: "'Roboto Slab', serif",
+        roboto: "'Roboto', sans-serif",
+      },
+      borderRadius: {
+        bord: "100px",
+      },
+      maxWidth: {
+        mxw: "1440px",
+      },
+    },
   },
   variants: {
     extend: {},
